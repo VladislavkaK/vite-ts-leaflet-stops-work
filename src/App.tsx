9067e9e7ce37cbd56a90391bb96b7e2e06bc1379
Vladislav Kuznetsov <super.ewew2015@yandex.ru>
2025-02-12 17:02:25 +0300

    added example map

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import L from "leaflet";
+
 import './App.scss';
+
+import markerIconPng from "leaflet/dist/images/marker-icon.png";
+import markerShadowPng from "leaflet/dist/images/marker-shadow.png";
+
+const customIcon = new L.Icon({
+  iconUrl: markerIconPng,
+  shadowUrl: markerShadowPng,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+});
 
 function App() {
   return (
     <div>
-      App
+      <MapContainer center={[51.505, -0.09]} zoom={13} style={{ height: "400px", width: "100%" }}>
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        <Marker position={[51.505, -0.09]} icon={customIcon}>
+          <Popup>Привет! Это маркер.</Popup>
+        </Marker>
+      </MapContainer>
     </div>
   )
 }
